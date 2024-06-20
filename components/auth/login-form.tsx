5e6d3319dfa35c97fe4ee094f0959d496f4b0d41
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-20 20:22:37 +0530

    created submit

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import React from "react";
+import * as z from "zod";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { LoginSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
 import {
 	Form,
 	FormControl,
@@ -13,7 +18,23 @@ import {
 } from "@/components/ui/form";
 
 import { CardWrapper } from "./card-wrapper";
+import { Button } from "../ui/button";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+
 export const LoginForm = () => {
+	const form = useForm<z.infer<typeof LoginSchema>>({
+		resolver: zodResolver(LoginSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+		console.log(values);
+	};
+
 	return (
 		<CardWrapper
 			headerLabel=' Welcome back'
@@ -21,7 +42,51 @@ export const LoginForm = () => {
 			backButtonLabel="don't have an account"
 			showSocial
 		>
-			Login form
+			<Form {...form}>
+				{/* will create this funciton up for submit
+				<form onSubmit={form.handleSubmit(() => {})} className='space-y-6'> 
+				 */}
+				<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+					<div className='space-y-4'>
+						<FormField
+							control={form.control}
+							name='email'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder='navdeep@example.com'
+											type='email'
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={form.control}
+							name='password'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input {...field} placeholder='*****' type='password' />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<FormError message='Invalid credentails! ' />
+					<FormSuccess message='Email sent! ' />
+					<Button type='submit' className='w-full'>
+						LogIn
+					</Button>
+				</form>
+			</Form>
 		</CardWrapper>
 	);
 };
